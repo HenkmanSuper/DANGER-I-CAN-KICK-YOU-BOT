ea3b01f70171a6dc7246d995cabb2d357d90948c
pk88 <42702011+superpieter88@users.noreply.github.com>
2018-09-10 18:38:39 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+import { Client } from "discord.js";
+import { prefix as _prefix, token } from "./botconfig.json";
+
+const bot = new Client();
+
+
+bot.on("ready", async () => {
+
+console.log(`${bot.user.username} is online!`);
+
+});
+
+
+bot.on("massage", async massage =>{
+
+if(massage.author.bot) return;
+
+if(massage.channel.typ === "dm") return;
+
+var prefix = _prefix;
+
+var massageArray = massage.content.split(" ");
+
+var commend = MessageArray[0];
+
+var arguments = MessageArray.slice(1);
+
+if(commend === `${prefix}hallo`)
+
+return massage.channel.send("HALLO!!");
+
+
+
+});
+
+
+bot.login(token);
