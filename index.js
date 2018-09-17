674824104ef2a38ce86e36f2dcafb734d7b1f03b
pk88 <42702011+superpieter88@users.noreply.github.com>
2018-09-17 19:44:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
+const Discord = require('discord.js')
+const bot = new Discord.Client({disableEveryone: true})
+client.login(process.env.token)
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`Hey, Wassup!`);
-});
 
-bot.on("message", async message => {
 
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
 
-  let prefix = '-';
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
 
-  if (cmd === `${prefix}ping`){
-    message.channel.send("Pong!");
-  }
-});
+  client.on('message', message => { 
+  var prefix = '!#!'
+  if(message.content.toLowerCase() ===  `${prefix}gevaar`){
+     
+    message.channel.send('IK BEN ONLINE BAAS!')
+    
+}
+   
+ 
+   
+   
+})
 
-bot.login(process.env.token);
+
+
+
+
+
+  client.on('message', message => { 
+  var prefix = ''
+  if(message.content.toLowerCase() ===  `${prefix}gevaar`){
+     
+    message.channel.send('DANGER!! DANGER!!')
+    message.channel.send('ER IS GEVAAR!!')
+    message.channel.send('RED JE ZELF!!')
+    
+}
+   
+ 
+   
+   
+})
+
+
+
+
+
+
+  client.on('message', message => { 
+  var prefix = ''
+  if(message.content.toLowerCase() ===  `${prefix}hallo`){
+     
+    message.channel.send('.HALLO!!')
+    message.channel.send('.zeg hallo super pieter!')
+    
+}
+   
+ 
+   
+   
+})
+
